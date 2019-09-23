6231bf40a03f82c230c6e2f6839ed8f3f1c3fbe5
Zahraa  Jalaa <zahraa.jalaa99@gmail.com>
2019-09-23 14:39:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 function fetchBooks() {
-
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(resp => resp.json())
+    .then(json => renderBooks(json))
 }
-
 function renderBooks(json) {
   const main = document.querySelector('main')
   json.forEach(book => {
