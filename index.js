87f31c8fa613bf3659cec040dc0ae96530432fdf
Zahraa  Jalaa <zahraa.jalaa99@gmail.com>
2019-09-23 14:26:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function fetchBooks() {
-fetch('https://anapioficeandfire.com/api/books')
+fetch("https://anapioficeandfire.com/api/books")
   .then(function(response){
     return response.json();
   })
